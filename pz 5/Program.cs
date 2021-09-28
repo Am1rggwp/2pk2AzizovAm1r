fb1a7a317774fbc2eb66f8754552bb9a6a838aca
Амир Азизов <amir.azizov2015@mail.ru>
2021-09-28 15:05:23 +0500

    Добавьте файлы проекта.

---
+++ pz 5/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace pz_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Задание №1:");
+            int a;
+            for (a = 0; a <= 90; a += 5)
+            {
+                Console.WriteLine(a);
+            }
+            Console.WriteLine();
+
+  
+            Console.WriteLine("Задание №2:");
+            char simvol;
+            int c = 1;
+            for (simvol = 'b'; c < 6; simvol++, c++)
+            {
+                Console.WriteLine(simvol);
+            }
+            
+
+            Console.WriteLine("Задание №3:");
+            for (int n = 0; n < 4; n++)
+            {
+                for (int m = 0; m < 5; m++)
+                {
+                    Console.Write("#");
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine();
+
+            Console.WriteLine("Задание №4:");
+            for (int x = 0; x <= 100; x++)
+            {
+                if (x % 7 == 0)
+                {
+                    Console.Write(x + " ");
+                }
+            }
+            Console.WriteLine();
+
+            Console.WriteLine("Задание №5:");
+            for (int i = 1 , j = 39; (i - j) != 20; i++, j--)
+            {
+                Console.WriteLine("{0} {1}", i, j);
+            }
+            Console.WriteLine();
+        }
+    }
+}
