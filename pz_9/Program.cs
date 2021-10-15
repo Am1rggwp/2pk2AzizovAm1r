6660b54eb47389bad932fd87481b7ff9666a2e3d
Амир Азизов <amir.azizov2015@mail.ru>
2021-10-15 13:34:47 +0500

    масив

---
+++ pz_9/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace pz_9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            char[,] arr = new char[8, 6];
+            char simvol = 'a';
+            int i, j;
+            for (i = 0; i < 8; i++)
+            {
+                for (j = 0; j < 6; j++)
+                {
+                    arr[i, j] = simvol++;
+                    Console.Write(arr[i, j] + " ");
+
+
+                }
+                Console.WriteLine();
+
+            }
+            Console.WriteLine();
+            for (i = 0; i < 6; i++)
+            {
+                char a = arr[0, i];
+                arr[0, i] = arr[7, i];
+                arr[7, i] = a;
+
+            }
+            for (i = 0; i < 8; i++)
+            {
+                for (j = 0; j < 6; j++)
+                {
+                    Console.Write(arr[i, j] + " ");
+
+
+                }
+                Console.WriteLine();
+
+            }
+
+
+        }
+    }
+
+}
