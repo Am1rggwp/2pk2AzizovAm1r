21a804301f617dbf213628b129fcd704df069ec1
Амир Азизов <amir.azizov2015@mail.ru>
2021-11-11 20:57:22 +0500

    ZzZzzZZpz_10

---
+++ PZ_12/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace PZ_12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[][] str = new string[3][];
+            str[0] = new string[3] { "Зеленый", "кустарный", "дом" };
+            str[1] = new string[3] { "big", "red", "apple" };
+            str[2] = new string[3] { "маленький", "yellow", "грибочек" };
+            for (int x = 0; x < 3; x++)
+            {
+                for (int y = 0; y < 3; y++)
+                {
+                    Console.Write(str[y][x] + " ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
