2dff8a125bd0bf7501ec33dca1011a1b1b1a7b65
Амир Азизов <amir.azizov2015@mail.ru>
2021-11-23 19:14:58 +0500

    14PZ

---
+++ PZ_14/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.IO;
+
+namespace PZ_14
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"C:\Text.txt";//Путь к файлу
+            if (!File.Exists(path)) // Проверяем на существование
+            {
+                // Создаём и записывам в этот файл
+                using (StreamWriter sw = File.CreateText(path))
+                {
+                    sw.WriteLine("Hello");
+                    sw.WriteLine("And");
+                    sw.WriteLine("Welcome");
+                }
+            }
+            Console.Write("Введите число длы вырезки: ");
+            int K = int.Parse(Console.ReadLine());
+            string str = "";
+            using (StreamReader sr = File.OpenText(path))// Открываем и читаем файл
+            {
+                string s;
+                while ((s = sr.ReadLine()) != null)
+                {
+                    K--; //Для простоты, чтобы не создавать несколько перемен, мы уменьшаем число
+                    if (K == 0) continue; //Если оно равно 0 - то это та позиция, которую надо вырезать
+                    str += s + "\n";
+                }
+            }
+            using (StreamWriter sw = File.CreateText(path))
+            {
+                sw.Write(str);
+            }
+        }
+    }
+}
