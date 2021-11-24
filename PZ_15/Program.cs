bfa1d19de8552e993fe4e82a5cafcb36aae816c9
Амир Азизов <amir.azizov2015@mail.ru>
2021-11-24 20:23:45 +0500

    PZ_15

---
+++ PZ_15/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.IO;
+
+namespace PZ_15
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = "C:/Text.txt";//путь к файлу
+            string[] str = File.ReadAllLines(path);//создаем масив который будет читать файл
+            for (int i = 0; i < str.Length; i++)//цыкл который прогонятет текст
+            {
+                char[] ch = str[i].ToCharArray();//массив с одним сиволом
+                if (ch[0] == ch[ch.Length - 1]) Console.WriteLine(str[i]);//При нахождении строчки с одним и тем же сиволом(в начале и в конце) выводит на экран 
+            }
+        }
+    }
+}
