8e47122a71b7063a32b590d3042eddb63ff17825
Амир Азизов <amir.azizov2015@mail.ru>
2021-11-30 19:40:05 +0500

    PZ_17

---
+++ PZ_17/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace PZ_17
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s = Console.ReadLine();//считываем строку
+            double a = CountSpace(s);
+            Console.WriteLine("Пробелов - " + a + "%");//выводим значение
+
+        }
+        static int CountSpace(string str)// сам метод
+        {
+            int result = 0;//счетчик пробелов
+            char[] c = str.ToCharArray();
+            for (int i = 0; i < c.Length; i++)//проверяем каждый символ строки соответсвует ли пробелу
+            {
+                if (c[i] == ' ')
+                {
+                    result++;
+                }
+            }
+            return result * 100 / c.Length;//формула расчета в процентном соотношении
+        }
+
+    }
+
+}
