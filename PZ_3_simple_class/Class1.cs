a889938933a98baa03eecbb1e989af39134981cf
Амир Азизов <amir.azizov2015@mail.ru>
2022-02-10 13:04:22 +0500

    Простой класс

---
+++ PZ_3_simple_class/Class1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PZ_3_simple_class
+{
+    static class ProgramP 
+    {
+        static void Main(string[] args)
+        {
+            Computer comp1 = new Computer(228337, 32, 3);
+            Computer comp2 = new Computer(233832, 16, 2);
+            Computer comp3 = new Computer(974359, 8, 4);
+            Computer comp4 = new Computer(753487, 4);
+
+            comp1.GetCompName();
+            comp2.GetCompName();
+            comp3.GetCompName();
+            comp4.GetCompName();
+
+
+
+        }
+
+
+    }
+}
---
+++ PZ_3_simple_class/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+class Computer
+{
+    public int ID;
+    public int totalHDDMemory;
+    public int processorFrequency;
+
+    public Computer (int a, int b, int c = 10)
+    {
+        ID = a;
+        totalHDDMemory = b;
+        processorFrequency = c;
+    } 
+    
+    public void GetCompName()
+    {
+        Console.WriteLine($"ID = {ID}, totalHDDMemory = {totalHDDMemory} Гб, processorFrequency = {processorFrequency} Гц ");
+    }
+
+
+}
