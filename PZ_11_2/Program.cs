763b89000a91389227b630bf8b0374e42cceafe4
Амир Азизов <amir.azizov2015@mail.ru>
2022-05-03 15:08:05 +0500

    ZP_11_2simestr

---
+++ PZ_11_2/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace PZ_11_2
+{
+ 
+    struct pz_11_2
+    {
+    
+        public char name;
+        public int chislo;
+
+        //метод для вывода информации по текущему экземпляру структуры
+        public void Show()
+        {
+            Console.WriteLine($"Символ -  {name} \nсумма - {chislo}");
+        }
+    }
+    internal class Program
+    {
+        unsafe static void Main(string[] args)
+        {
+            pz_11_2 obj1, obj2;   // создание экземпляров структур
+            pz_11_2* allobj;      // создание указателя на структуру
+
+            allobj = &obj1;  
+
+            // обращение к полям 
+            allobj->name = 'A';
+            allobj->chislo = 1000;
+
+            allobj->Show();     // вызов метода структуры 
+
+            allobj = &obj2;   // новое значение
+
+            // обращение к полям структуры 
+            allobj->name = 'B';
+            allobj->chislo = 500;
+
+            allobj->Show();     // вызов метода 
+
+          
+
+          
+                                
+        }
+    }
+   
+}
